8c3af267cfe3a6d94244362fbc8387150a3b6d22
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2024-08-03 18:13:30 +0200

    Apply local thresholding on larger sample

--- proc/run_3d_segmentation_embryo.py
+++ proc/run_3d_segmentation_embryo.py
@@ -16,6 +16,7 @@ import skimage as ski
 import zarr as zr
 
 
+# Use proper prefix to locate data files
 if len(sys.argv) > 1:
     pref = sys.argv[1]
 else:
@@ -34,21 +35,20 @@ print(f'The shape of the image is: {im3d.shape}')
 # Trim 3D image so that each axis is a multiple of 50.
 
 im3d = im3d[:800, :2000, :2150]
-sample = im3d[400:500, :1500, :1000]
+sample = im3d[400:500, :, :1500]
 print(f'The shape of the sample is: {sample.shape}')
 
 #####################################################################
-# Apply thresholding techniques
-# =============================
+# Apply local thresholding
+# ========================
 
-# global thresholding
-def binary_global(x):
-    global_thresh = ski.filters.threshold_otsu(x)
-    return (x > global_thresh)
+# # global thresholding
+# def binary_global(x):
+#     global_thresh = ski.filters.threshold_otsu(x)
+#     return (x > global_thresh)
+#
+# gl = ski.util.apply_parallel(binary_global, im3d, chunks=50)
 
-gl = ski.util.apply_parallel(binary_global, sample, chunks=50)
-
-# local thresholding
 local_thresh = ski.util.apply_parallel(
     ski.filters.threshold_local,
     sample,
