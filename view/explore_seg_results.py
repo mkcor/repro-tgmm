0bc55828881813fcd15e3f9961cf4f30e410a0ca
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2024-08-27 15:51:57 +0200

    Customize display in napari
    Co-authored-by: Wouter-Michiel Vierdag <w-mv@hotmail.com>

--- view/explore_seg_results.py
+++ view/explore_seg_results.py
@@ -8,10 +8,30 @@ proc_seg = np.load('proc_seg.npz')['arr_0']
 tgmm_seg = np.load('tgmm_seg.npz')['arr_0']
 
 viewer = napari.Viewer()
-image_layer = viewer.add_image(sample_slice, name='raw image')
-tgmm_layer = viewer.add_labels(tgmm_seg, name='reproducibility')
-proc_layer = viewer.add_labels(proc_seg, name='replication')
-cells_layer = viewer.add_image(cells, name='cells before watershed')
+image_layer = viewer.add_image(
+    sample_slice,
+    blending='additive',
+    contrast_limits=(0, 350),
+    name='raw image'
+)
+tgmm_layer = viewer.add_labels(
+    tgmm_seg,
+    blending='translucent',
+    name='reproducibility'
+)
+cells_layer = viewer.add_image(
+    cells,
+    blending='additive',
+    name='cells before watershed'
+)
+proc_layer = viewer.add_labels(
+    proc_seg,
+    blending='translucent',
+    name='replication'
+)
+viewer.grid.enabled = True
+viewer.grid.stride = 2
+viewer.dims.current_step = (50, 999, 1074)
 
 # start the event loop and show the viewer
 napari.run()
